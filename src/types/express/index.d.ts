d398c90b440cb10703fb9c03966cdacd7cf15468
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-07-31 11:51:03 -0600

    Fix: type Request solucionado

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,11 +1,14 @@
-import { type User } from "../types/type";
+// Esto nos permite añadir propiedades personalizadas al objeto Request de Express
+// y que TypeScript sea consciente de ellas.
 
 declare global {
 	namespace Express {
-		interface Request {
-			user?: User;
+		export interface Request {
+			// Añadimos la propiedad 'user' a la interfaz Request.
+			// Es opcional (?) porque no todas las peticiones la tendrán (solo las autenticadas).
+			user?: { username: string; role: string };
 		}
 	}
 }
 
-export default {}
+export {}
