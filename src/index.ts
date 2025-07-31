efe758d93f85248f742da100cdc456e62405f6f8
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-07-30 19:35:26 -0600

    Install dependencias

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // src/index.ts
 import express, { Express, Request, Response } from 'express';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
