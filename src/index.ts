ade10a647a7278e8e8d0531d7e9dea2d0face557
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-07-30 19:27:03 -0600

    Init Projects

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// src/index.ts
+import express, { Express, Request, Response } from 'express';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('¡Hola, mundo con TypeScript!');
+});
+
+app.listen(port, () => {
+	console.log(`[server]: Server is running at http://localhost:${port}`);
+});
