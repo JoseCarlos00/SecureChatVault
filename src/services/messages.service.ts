7bb49d4932fcf65925ba071446495b92cfa604e4
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-08-13 11:05:29 -0600

    Fix: message.service: devolver todos los campos del mensaje ligado con populate(´replyTo')

--- src/services/messages.service.ts
+++ src/services/messages.service.ts
@@ -41,7 +41,7 @@ export const getMessagesFromDB = async (options: GetMessagesOptions) => {
 	const messages = await MessageModel.find(query)
 		.sort({ _id: -1 }) // más recientes primero
 		.limit(limit)
-		.populate('replyTo', 'sender content caption')
+		.populate('replyTo')
 		.exec();
 
 	return { messages: messages.reverse(), total };
