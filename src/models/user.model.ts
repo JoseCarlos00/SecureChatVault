d074a1ee256e6289829eb82e3a93e156ecbc3e35
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-08-25 10:57:20 -0600

    Feat: hacer que el método comparePassword sea asíncrono para mejorar la comparación de contraseñas

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -12,14 +12,15 @@ export interface UserDocument extends Omit<User, '_id'>, Document {
 // Middleware (hook) para hashear la contraseña antes de guardarla
 UserSchema.pre<UserDocument>('save', async function (next) {
 	if (!this.isModified('password')) return next();
+
 	const salt = await bcrypt.genSalt(10);
 	this.password = await bcrypt.hash(this.password, salt);
 	next();
 });
 
 // Método para comparar contraseñas
-UserSchema.methods.comparePassword = function (password: string): Promise<boolean> {
-	return bcrypt.compare(password, this.password);
+UserSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
+	return await bcrypt.compare(password, this.password);
 };
 
 export const UserModel = model<UserDocument>('User', UserSchema);
