868960584ee68c37686f354b1c6e198326b10dd8
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-08-25 12:11:48 -0600

    Feat: agregar selección de contraseña al buscar usuario por ID

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,5 +2,5 @@ import { UserModel } from "../models/user.model"
 import type { User } from "../types/user";
 
 export const findUserById = async (id: string): Promise<User | null>  => {
-  return await UserModel.findById(id);
+  return await UserModel.findById(id).select('+password');
 }
