cbced58470e5767f95cab18e598c7186246adac7
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-08-29 19:20:40 -0600

    Fix: convertir un TextMessage con as TextMessage

--- src/services/parser.service.ts
+++ src/services/parser.service.ts
@@ -1,4 +1,4 @@
-import { Message, TextMessage, MediaMessage } from '../types/message';
+import type { Message, TextMessage, MediaMessage } from '../types/message';
 
 // Expresión regular mejorada para manejar el espacio después de la hora
 const messageRegex = /^(\d{1,2}\/\d{1,2}\/\d{2,4}),\s(\d{1,2}:\d{2})\s?(am|pm)\s?-\s(.*?):\s(.*)/;
@@ -116,7 +116,7 @@ const createMessageObject = (
 			...commonProps,
 			type: 'text',
 			content: cleanContent,
-		};
+		} as TextMessage;
 	} else {
 		const filename = content.split(' ').slice(0, -2).join(' ');
 
